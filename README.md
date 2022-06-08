74f55f9f4d75340103defd92d4a0dfcdd4dddf09
〽️ Sam! XD <106489677+Sam-XD7@users.noreply.github.com>
2022-06-08 10:22:35 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,40 +1,31 @@
-# tguploaderpro
-Bot De Telegram : TGUploaderPro v7.0 Fixed , Descargador gratis de contenido desde internet a hacia moodles , nexcloud en cuba
+# TG-Uploader X
 
-# Deploy Usando Git Win Y Heroku Cli Desde PC
-```
-(CMD)
-git clone https://github.com/ObisoftDev/tguploaderpro 
-git init
-git add .
-git commit -m "OK"
-heroku create myherokuapp
-heroku git:remote myherokuapp
-git push heroku master
-```
+Sube archivos a Nextcloud y Moodles directamente desde Telegram.
 
-# Comandos En El Bot (Usuarios Nomales)
-```/start : Inicar Bot , Te Da La INfo
-/tutorial : Te Da un tutorial basico de uso del bot q puedes echarle un ojo
-/myuser : Obtiene la informacion del usuario q esta usando el bot
-/zips : Configura el tamano de las partes comprimidas 7z
-/account: COnfigura su cuenta de nube en el bot
-/host : Configura el Host Al Cual ba a subir los archivos el bot x ejemplo https://moodle.uclv.edu.cu/ (Moodle o Nexcloud)
-/repoid : EN EL caso de las moodles cada nube tiene su repoid q hay q saber extraerlo y configurarsel al bot para poder subir
-/cloud : Alterna El tipo de subida a nubes ya sea cloud o moodle , en caso de cloud es nexcloud pero para simplificar se pone cloud
-/tokenize_on : Enciende el modo tokenize , se recomienda no usar a no se q disponga de una de las apps oficiales de descarga del bot 
-/tokenize_off : Apaga el modo tokenize
-/uptype : Configure el modo de subir de moodle ya sea draft , evidence , blog y calendario
-/proxy : Configura UN Proxy Para Las Subidas Del Bot , contactar en telegram a @obisoftdevel para contratar uno
-/files : En caso de tener activa el uptype (evidence) este comando le da una lista de archivo q se encuentra en las evidencias de la nube
-/delall : En caso de tener activa el uptype (evidence) este comando borra todos los archivos en la lista de evidencia de la nube
-/dir : En caso de tener activo cloud configure el directorio base en la nexcloud donde se va a subir los archivos
-```
+- `1` - En Telegram inicia el bot [@BotFather](https://t.me/BotFather), crea un bot y copia el token.
+- `2` - En el código abrir el archivo [main.py](/main.py).
+- `3` - En la linea 243 rellenar (tl_admin_user = 'usuario de telegram sin el @').
 
-# Comandos En El Bot (Administrador) 
-```/adduser : permite un usuario de telegram tener acceso al bot
-/banuser : quita acceso al bot de un usuario de telegram
-/getdb : Obten la base de datos donde se almacenan la info de los usarios en el bot
-```
-# Deploy Directo (Heroku)
-[![Heroku Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/ObisoftDev/tguploaderpro)
+## Desplegar en Heroku
+
+[![Deploy on Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Comandos
+Agregar en [@BotFather](https://t.me/BotFather)
+
+    start - iniciar bot, te da la info.
+    myuser - obtiene la información del usuario que está usando el bot.
+    account - configura su cuenta de nube en el bot.
+    host - configura el host al cuál va a subir los archivos el bot.
+    repoid - en el caso de las moodles cada nube tiene su repoid que hay que saber extraerlo y configurarselo al bot para poder subir.
+    cloud - alterna el tipo de subida a nubes ya sea cloud o moodle, en caso de cloud es nexcloud pero para simplificar se pone cloud.
+    uptype - configure el modo de subir al moodle ya sea draft, evidence, blog o calendar.
+    zips - configura el tamaño de las partes comprimidas 7z.
+    proxy - configura un proxy para las subidas del bot.
+    files - en caso de tener activo el uptype (evidence) este comando le da una lista de archivos que se encuentran en las evidencias de la nube.
+    delall - en caso de tener activo el uptype (evidence) este comando borra todos los archivos en la lista de evidencia de la nube.
+    dir - en caso de tener activo cloud configure el directorio base en la nexcloud donde se van a subir los archivos.
+    adduser - permite un usuario de telegram tener acceso al bot.
+    banuser - quita acceso al bot a un usuario de telegram.
+    getdb - obtén la base de datos donde se almacenan la info de los usarios del bot.
+    version - muestra la versión actual.
--- infos.py
+++ infos.py
@@ -12,12 +12,12 @@ def text_progres(index,max):
 		porcent = round(porcent)
 		make_text = ''
 		index_make = 1
-		make_text += '\n['
+		make_text += '\n'
 		while(index_make<21):
-			if porcent >= index_make * 5: make_text+='●'
-			else: make_text+='○'
+			if porcent >= index_make * 5: make_text+='█'
+			else: make_text+='░'
 			index_make+=1
-		make_text += ']\n'
+		make_text += '\n'
 		return make_text
 	except Exception as ex:
 			return ''
@@ -29,78 +29,78 @@ def porcent(index,max):
     return porcent
 
 def createDownloading(filename,totalBits,currentBits,speed,time,tid=''):
-    msg = '📥Descargando... \n\n'
-    msg+= '🔖Nombre: ' + str(filename)+'\n'
-    msg+= '🗂Tamaño Total: ' + str(sizeof_fmt(totalBits))+'\n'
-    msg+= '🗂Descargado: ' + str(sizeof_fmt(currentBits))+'\n'
-    msg+= '📶Velocidad: ' + str(sizeof_fmt(speed))+'/s\n'
-    msg+= '🕐Tiempo: ' + str(datetime.timedelta(seconds=int(time))) +'\n\n'
+    msg = '⬇  Descargando...\n'
+    msg+= '🏷 Nombre: ' + str(filename)+'\n'
+    msg+= '📦 Tamaño total: ' + str(sizeof_fmt(totalBits))+'\n'
+    msg+= '🔻 Descargado: ' + str(sizeof_fmt(currentBits))+'\n'
+    msg+= '🚀 Velocidad: ' + str(sizeof_fmt(speed))+'/s\n'
+    msg+= '⏰ Tiempo: ' + str(datetime.timedelta(seconds=int(time))) +'\n\n'
 
-    msg = '📡 Descargando Archivo....\n\n'
-    msg += '➤ Archivo: '+filename+'\n'
+    msg = '⬇  Descargando...\n'
+    msg += '🏷  Archivo: '+filename+'\n'
     msg += text_progres(currentBits,totalBits)+'\n'
-    msg += '➤ Porcentaje: '+str(porcent(currentBits,totalBits))+'%\n\n'
-    msg += '➤ Total: '+sizeof_fmt(totalBits)+'\n\n'
-    msg += '➤ Descargado: '+sizeof_fmt(currentBits)+'\n\n'
-    msg += '➤ Velocidad: '+sizeof_fmt(speed)+'/s\n\n'
-    msg += '➤ Tiempo de Descarga: '+str(datetime.timedelta(seconds=int(time)))+'s\n\n'
+    msg += '📈 Progreso: '+str(porcent(currentBits,totalBits))+'%\n'
+    msg += '📦 Tamaño total: '+sizeof_fmt(totalBits)+'\n'
+    msg += '🔻 Descargado: '+sizeof_fmt(currentBits)+'\n'
+    msg += '🚀 Velocidad: '+sizeof_fmt(speed)+'/s\n'
+    msg += '⏰ Tiempo restante: '+str(datetime.timedelta(seconds=int(time)))+'s\n\n'
 
     if tid!='':
         msg+= '/cancel_' + tid
     return msg
 def createUploading(filename,totalBits,currentBits,speed,time,originalname=''):
-    msg = '⏫Subiendo A La Nube☁... \n\n'
-    msg+= '🔖Nombre: ' + str(filename)+'\n'
+    msg = '⬆️ Subiendo...\n'
+    msg+= '🏷 Nombre: ' + str(filename)+'\n'
     if originalname!='':
         msg = str(msg).replace(filename,originalname)
-        msg+= '⏫Subiendo: ' + str(filename)+'\n'
-    msg+= '🗂Tamaño Total: ' + str(sizeof_fmt(totalBits))+'\n'
-    msg+= '🗂Subido: ' + str(sizeof_fmt(currentBits))+'\n'
-    msg+= '📶Velocidad: ' + str(sizeof_fmt(speed))+'/s\n'
-    msg+= '🕐Tiempo: ' + str(datetime.timedelta(seconds=int(time))) +'\n'
+        msg+= '⬆️ Subiendo: ' + str(filename)+'\n'
+    msg+= '📦 Tamaño total: ' + str(sizeof_fmt(totalBits))+'\n'
+    msg+= '🔺 Subido: ' + str(sizeof_fmt(currentBits))+'\n'
+    msg+= '🚀 Velocidad: ' + str(sizeof_fmt(speed))+'/s\n'
+    msg+= '⏰ Tiempo: ' + str(datetime.timedelta(seconds=int(time))) +'\n'
 
-    msg = '⏫ Subiendo A La Nube☁...\n\n'
-    msg += '➤ Nombre: '+filename+'\n'
+    msg = '⬆️ Subiendo...\n'
+    msg += '🏷 Nombre: '+filename+'\n'
     if originalname!='':
         msg = str(msg).replace(filename,originalname)
-        msg+= '➤ Nombre: ' + str(filename)+'\n'
+        msg+= '🗜 Subiendo: ' + str(filename)+'\n'
     msg += text_progres(currentBits,totalBits)+'\n'
-    msg += '➤ Porcentaje: '+str(porcent(currentBits,totalBits))+'%\n\n'
-    msg += '➤ Total: '+sizeof_fmt(totalBits)+'\n\n'
-    msg += '➤ Descargado: '+sizeof_fmt(currentBits)+'\n\n'
-    msg += '➤ Velocidad: '+sizeof_fmt(speed)+'/s\n\n'
-    msg += '➤ Tiempo de Descarga: '+str(datetime.timedelta(seconds=int(time)))+'s\n\n'
+    msg += '📈 Progreso: '+str(porcent(currentBits,totalBits))+'%\n'
+    msg += '📦 Tamaño total: '+sizeof_fmt(totalBits)+'\n'
+    msg += '🔺 Subido: '+sizeof_fmt(currentBits)+'\n'
+    msg += '🚀 Velocidad: '+sizeof_fmt(speed)+'/s\n'
+    msg += '⏰ Tiempo restante: '+str(datetime.timedelta(seconds=int(time)))+'s\n\n'
 
     return msg
 def createCompresing(filename,filesize,splitsize):
-    msg = '📚Comprimiendo... \n\n'
-    msg+= '🔖Nombre: ' + str(filename)+'\n'
-    msg+= '🗂Tamaño Total: ' + str(sizeof_fmt(filesize))+'\n'
-    msg+= '📂Tamaño Partes: ' + str(sizeof_fmt(splitsize))+'\n'
-    msg+= '💾Cantidad Partes: ' + str(round(int(filesize/splitsize)+1,1))+'\n\n'
+    msg = '🗜 Comprimiendo...\n'
+    msg+= '🏷 Nombre: ' + str(filename)+'\n'
+    msg+= '📦 Tamaño total: ' + str(sizeof_fmt(filesize))+'\n'
+    msg+= '🗜 Tamaño de partes: ' + str(sizeof_fmt(splitsize))+'\n'
+    msg+= '#⃣ Cantidad de partes: ' + str(round(int(filesize/splitsize)+1,1))+'\n\n'
     return msg
 def createFinishUploading(filename,filesize,split_size,current,count,findex):
-    msg = '📌Proceso Finalizado📌\n\n'
-    msg+= '🔖Nombre: ' + str(filename)+'\n'
-    msg+= '🗂Tamaño Total: ' + str(sizeof_fmt(filesize))+'\n'
-    msg+= '📂Tamaño Partes: ' + str(sizeof_fmt(split_size))+'\n'
-    msg+= '📤Partes Subidas: ' + str(current) + '/' + str(count) +'\n\n'
-    msg+= '🗑Borrar Archivo: ' + '/del_'+str(findex)
+    msg = '<b>✅ Subida Completada</b>\n'
+    msg+= '🏷 Nombre: ' + str(filename)+'\n'
+    msg+= '📦 Tamaño total: ' + str(sizeof_fmt(filesize))+'\n'
+    msg+= '🗜 Tamaño de partes: ' + str(sizeof_fmt(split_size))+'\n'
+    msg+= '#⃣ Cantidad de partes: ' + str(current) + '/' + str(count) +'\n\n'
+    msg+= '🗑 Borrar Archivo: ' + '/del_'+str(findex)
     return msg
 
 def createFileMsg(filename,files):
     import urllib
     if len(files)>0:
-        msg= '<b>🖇Enlaces🖇</b>\n'
+        msg= '<b>🔗 Enlaces</b>\n'
         for f in files:
             url = urllib.parse.unquote(f['directurl'],encoding='utf-8', errors='replace')
-            #msg+= '<a href="'+f['url']+'">🔗' + f['name'] + '🔗</a>'
-            msg+= "<a href='"+url+"'>🔗"+f['name']+'🔗</a>\n'
+            #msg+= '<a href="'+f['url']+'">' + f['name'] + '</a>'
+            msg+= "<a href='"+url+"'>"+f['name']+'</a>\n'
         return msg
     return ''
 
 def createFilesMsg(evfiles):
-    msg = '📑Archivos ('+str(len(evfiles))+')📑\n\n'
+    msg = '📑 Archivos ('+str(len(evfiles))+') 📑\n\n'
     i = 0
     for f in evfiles:
             try:
@@ -117,29 +117,29 @@ def createFilesMsg(evfiles):
     return msg
 def createStat(username,userdata,isadmin):
     from pyobigram.utils import sizeof_fmt
-    msg = '⚙️Condiguraciones De Usuario⚙️\n\n'
-    msg+= '🔖Nombre: @' + str(username)+'\n'
-    msg+= '📑User: ' + str(userdata['moodle_user'])+'\n'
-    msg+= '🗳Password: ' + str(userdata['moodle_password'])+'\n'
-    msg+= '📡Host: ' + str(userdata['moodle_host'])+'\n'
+    msg = '⚙ Configuración de Usuario ⚙\n\n'
+    msg+= '📛 Nombre: @' + str(username)+'\n'
+    msg+= '👤 User: ' + str(userdata['moodle_user'])+'\n'
+    msg+= '🔐 Password: ' + str(userdata['moodle_password'])+'\n'
+    msg+= '🌐 Host: ' + str(userdata['moodle_host'])+'\n'
     if userdata['cloudtype'] == 'moodle':
-        msg+= '🏷RepoID: ' + str(userdata['moodle_repo_id'])+'\n'
-    msg+= '🏷CloudType: ' + str(userdata['cloudtype'])+'\n'
-    msg+= '📟UpType: ' + str(userdata['uploadtype'])+'\n'
+        msg+= '🆔 RepoID: ' + str(userdata['moodle_repo_id'])+'\n'
+    msg+= 'Ⓜ CloudType: ' + str(userdata['cloudtype'])+'\n'
+    msg+= '🧰 UpType: ' + str(userdata['uploadtype'])+'\n'
     if userdata['cloudtype'] == 'cloud':
-        msg+= '🗂Dir: /' + str(userdata['dir'])+'\n'
-    msg+= '📚Tamaño de Zips : ' + sizeof_fmt(userdata['zips']*1024*1024) + '\n\n'
-    msgAdmin = 'No'
+        msg+= '📂 Dir: /' + str(userdata['dir'])+'\n'
+    msg+= '🗜 Tamaño de Zips: ' + sizeof_fmt(userdata['zips']*1024*1024) + '\n\n'
+    msgAdmin = 'NO'
     if isadmin:
-        msgAdmin = 'Si'
-    msg+= '🦾Admin : ' + msgAdmin + '\n'
+        msgAdmin = 'SI'
+    msg+= '👮 Admin: ' + msgAdmin + '\n'
     proxy = 'NO'
     if userdata['proxy'] !='':
        proxy = 'SI'
     tokenize = 'NO'
     if userdata['tokenize']!=0:
        tokenize = 'SI'
-    msg+= '🔌Proxy : ' + proxy + '\n'
-    msg+= '🔮Tokenize : ' + tokenize + '\n\n'
-    msg+= '⚙️Configurar Moodle⚙️\n🤜Ejemplo /account user,password👀'
+    msg+= '🔌 Proxy: ' + proxy + '\n'
+    msg+= '🔮 Tokenize: ' + tokenize + '\n\n'
+    msg+= '⚙ Configurar Moodle ⚙\n🛠 Ejemplo /account user,password'
     return msg
